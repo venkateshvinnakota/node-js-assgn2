045cf5d77c4a5264daf2634bc8f5570f768c1be8
venkatesh <venkey.vinnakota@gmail.com>
2022-12-15 08:43:39 +0000

    node js assgn2

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const dbPath = path.join(__dirname, "todoApplication.db");
+const app = express();
+
+app.use(express.json());
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({ filename: dbPath, driver: sqlite3.Database });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(-1);
+  }
+};
+initializeDBAndServer();
+
+app.get("/todos/", (request, response) => {
+  const { status } = request.query;
+  const getTodosQuery = `SELECT * FROM todo WHERE status=${status}`;
+});
